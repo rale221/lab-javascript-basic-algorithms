5ce6ea5075230632fbfe6ee377439ee66cfa97b9
Rastko <rastkodjurica@yahoo.com>
2021-01-13 23:59:09 -0500

    Rastko Basic Algorithms

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
 // Iteration 1: Names and Input
+let hacker1 = 'Kristina'
 
+console.log('The Drivers name is ' + hacker1);
+
+let hacker2 = 'Rastko'
+console.log('The navigators name is ' + hacker2);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length>hacker2.length) {
+    console.log('The driver has the longest name, it has ' + hacker1.length + ' characters.');
+
+} else if (hacker1.length<hacker2.length) {
+
+console.log('It seems that the navigator has the longest name, it has ' + hacker2.length + ' characters.');
+
+} else
+
+console.log('Wow, you both have equally long names, ' + hacker2.length + ' characters!');
+
+
 
 // Iteration 3: Loops
+
+hacker1 = hacker1.split('').join(' ');
+console.log(hacker1.toUpperCase());
+
+
